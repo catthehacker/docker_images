a39159b9899f16b0a50bfc7fbc8dab328b07ac1a
hackercat <me@hackerc.at>
2021-06-14 07:28:53 +0000

    fix: add `build.sh`

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+docker buildx build \
+    --pull \
+    --push \
+    --progress=plain \
+    --tag="ghcr.io/${SLUG}:${TYPE}-${TAG}" \
+    --tag="quay.io/${SLUG}:${TYPE}-${TAG}" \
+    --tag="docker.io/${SLUG}:${TYPE}-${TAG}" \
+    --build-arg="NODE_VERSION=${NODE}" \
+    --build-arg="DISTRO=${DISTRO}" \
+    --build-arg="BUILD_TAG_VERSION=${BUILD_TAG_VERSION}" \
+    --build-arg="BUILD_TAG=${BUILD_TAG}" \
+    --build-arg="BUILD_REF=${BUILD_REF}" \
+    --build-arg="FROM_IMAGE=${FROM_IMAGE}" \
+    --build-arg="FROM_TAG=${FROM_TAG}" \
+    --file="./linux/${IMAGE}/${TYPE}/Dockerfile" \
+    --platform="${PLATFORMS}" \
+    .
