040f776b09d7ff758b5ef8d838ee90befa822325
hackercat <me@hackerc.at>
2021-06-14 21:16:49 +0000

    refactor: stuff
    i'm too emotionally exhausted to describe changes, just read code

--- build.sh
+++ build.sh
@@ -4,9 +4,9 @@ docker buildx build \
     --pull \
     --push \
     --progress=plain \
-    --tag="ghcr.io/${SLUG}:${TYPE}-${TAG}" \
-    --tag="quay.io/${SLUG}:${TYPE}-${TAG}" \
-    --tag="docker.io/${SLUG}:${TYPE}-${TAG}" \
+    --tag="ghcr.io/${SLUG}:${TAG}" \
+    --tag="quay.io/${SLUG}:${TAG}" \
+    --tag="docker.io/${SLUG}:${TAG}" \
     --build-arg="NODE_VERSION=${NODE}" \
     --build-arg="DISTRO=${DISTRO}" \
     --build-arg="BUILD_TAG_VERSION=${BUILD_TAG_VERSION}" \
