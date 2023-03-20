443294eb36403d82b65a5163c97213e5cb2ffc8f
Chris Helmich <kagekirin@gmail.com>
2023-03-20 15:21:16 +0100

    refactor (gh.sh): install 'gh' through apt

--- linux/ubuntu/scripts/gh.sh
+++ linux/ubuntu/scripts/gh.sh
@@ -7,6 +7,7 @@
 # source: https://github.com/actions/virtual-environments/blob/be27ebfdb31aece2c90fbe1984c1749cbd1b464c/images/linux/scripts/installers/github-cli.sh
 
 # Install GitHub CLI
-url=$(curl -s https://api.github.com/repos/cli/cli/releases/latest | jq -r ".assets[].browser_download_url|select(contains(\"linux\") and contains(\"$(arch)\") and contains(\".deb\"))")
-wget -q "$url" -O "/tmp/gh.deb"
-apt install /tmp/gh.deb
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | sudo dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list > /dev/null
+sudo apt-get update  -y -qq
+sudo apt-get install gh
