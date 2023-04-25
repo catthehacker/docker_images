3b24dcbbfb0f6bf830d0c19d93542829e98a356a
ChristopherHX <christopher.homberger@web.de>
2023-04-25 13:39:12 +0000

    exclude lib32z1 for arm64

--- linux/ubuntu/scripts/basic.sh
+++ linux/ubuntu/scripts/basic.sh
@@ -6,7 +6,12 @@
 # source: https://github.com/actions/virtual-environments/blob/5ae2170ebe90a53e1cdc9c507ed3e0f1471d6b66/images/linux/scripts/helpers/install.sh
 
 apt-get update
-common_packages=$(jq -r ".apt.common_packages[]" "/imagegeneration/toolset.json")
+# lib32z1 is not available for arm64 remove it via jq
+case "$(uname -m)" in
+  'x86_64') common_packages_filter="" ;;
+  *) common_packages_filter="del(.apt.common_packages[] | select(. == \"lib32z1\"))" ;;
+esac
+common_packages=$(jq -r "$common_packages_filter .apt.common_packages[]" "/imagegeneration/toolset.json")
 cmd_packages=$(jq -r ".apt.cmd_packages[]" "/imagegeneration/toolset.json")
 # we depend on re-splitting behaviour here
 # shellcheck disable=SC2068
