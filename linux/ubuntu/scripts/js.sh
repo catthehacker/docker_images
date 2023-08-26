4f6bdf85e1124ebe5774b4b601ac9285b1ede4a8
ChristopherHX <christopher.homberger@web.de>
2023-08-26 22:22:05 +0200

    enable arm64 builds (#94)
    * enable arm64 builds
    
    * Add `[skip build-base]` conditional for PRs
    
    * only setup arm64 arch in qemu
    
    * fix permission java-tools
    
    * go default to 1.20 and fix pwsh
    
    * exclude lib32z1 for arm64
    
    * increase npm timeout for qemu
    
    * Try to speed up npm under qemu
    
    * make npm verbose
    
    * Use http registry
    
    * Another test
    
    * Use less build resources
    
    * fix dockerhub tag
    
    * Fix building

--- linux/ubuntu/scripts/js.sh
+++ linux/ubuntu/scripts/js.sh
@@ -42,11 +42,31 @@ for V in "${versions[@]}"; do
   "$NODEPATH/bin/node" -v
 done
 
+# npm timeout under qemu with defaults
+set -x
+npm config set fetch-timeout 120000
+npm config set fetch-retry-mintimeout 120000
+npm config set fetch-retry-maxtimeout 120000
+npm config set prefer-offline true
+npm config set registry http://registry.npmjs.org/
+# Otherwise there are no log updates for 10m+ on qemu
+npm config set loglevel verbose
+npm config ls -l
+
 printf "\n\t🐋 Installing JS tools 🐋\t\n"
 npm install -g npm
 npm install -g pnpm
 npm install -g yarn
-npm install -g grunt gulp n parcel-bundler typescript newman vercel webpack webpack-cli lerna
+npm install -g grunt
+npm install -g gulp
+npm install -g n
+npm install -g parcel-bundler
+npm install -g typescript
+npm install -g newman
+npm install -g vercel
+npm install -g webpack
+npm install -g webpack-cli
+npm install -g lerna
 npm install -g --unsafe-perm netlify-cli
 
 printf "\n\t🐋 Installed NPM 🐋\t\n"
@@ -61,4 +81,6 @@ yarn -v
 printf "\n\t🐋 Cleaning image 🐋\t\n"
 apt-get clean
 rm -rf /var/cache/* /var/log/* /var/lib/apt/lists/* /tmp/* || echo 'Failed to delete directories'
+# remove npm config
+npm config edit --editor rm
 printf "\n\t🐋 Cleaned up image 🐋\t\n"
