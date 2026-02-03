3eba278c6231fb846749ef4f2d66a9af7f4ea7d3
Jakub Panek <me@panekj.dev>
2026-02-04 00:47:34 +0100

    fix: shell language so good

--- linux/ubuntu/scripts/act.sh
+++ linux/ubuntu/scripts/act.sh
@@ -123,7 +123,7 @@ printf "\n\t🐋 Installed moby-buildx 🐋\t\n"
 docker buildx version
 IFS=' ' read -r -a NODE <<<"$NODE_VERSION"
 for ver in "${NODE[@]}"; do
-  if [[ "${ver}" == "24" && "$(node_arch)" == "armv7l"]]; then
+  if [[ "${ver}" == "24" && "$(node_arch)" == "armv7l" ]]; then
     ver="22" # rip arm32/v7
   fi
   printf "\n\t🐋 Installing Node.JS=%s 🐋\t\n" "${ver}"
